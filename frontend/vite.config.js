8a5912b5e8a4c0c45266323a1e15212878f61ad0
2300032653 <2300032653@kluniversity.in>
2026-02-21 23:35:59 +0530

    Fixed merge conflict in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   server: {
     port: 5174,
     host: true
   }
-=======
->>>>>>> e078af6b1b0fa6fbdfb4f7fbcad5fe841b84b186
 })
